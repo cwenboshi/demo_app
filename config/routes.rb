9cb6f6b69d642db091a7fb45b35d170b0ef5a0db
Wenbo <cwenboshi@gmail.com>
2014-07-03 18:06:46 -0700

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/home' => 'static_pages#home'
+  get '/help'=> 'static_pages#help'
+  get '/about'=> 'static_pages#about'
+
+  root 'static_pages#home'
+
   resources :microposts
 
   resources :users
